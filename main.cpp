ee6952a0c5ccc115dbe8ef20bae8f2b444a6d72f
BrenoBR1 <breno-hp2@hotmail.com>
2020-05-07 21:00:05 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main()
 {
+string sexo;
+int cerv, refr, espeto;
+cout << "Sexo: " << endl;
+cin >> sexo;
+cout << "Quantidade de cervejas: " << endl;
+cin >> cerv;
+cout << "Quantidade de refrigerantes: " << endl;
+cin >> refr;
+cout << "Quantidade de espetinhos: " << endl;
+cin >> espeto;
+return 0;
+
+
+
+
+
+
+
+
 
     }
